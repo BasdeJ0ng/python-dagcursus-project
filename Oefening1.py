7fbba191ceb01048a12a6df0fee207ca9f04ad40
Bas de Jong <bas.de.jong@handsonrisk.onmicrosoft.com>
2025-05-23 13:47:55 +0200

    test verwijderen bestand

--- Oefening1.py
+++
@@ -1,9 +0,0 @@
-voornaam = "Bas"
-leeftijd = 49
-leert_python = False
-if leert_python: 
-    wel_niet="wel" 
-else: 
-    wel_niet="niet"
-    
-print(voornaam, "probeert",wel_niet,"Python te leren en is",leeftijd,"jaar oud")
